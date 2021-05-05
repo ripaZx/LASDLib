abfc54ecc250802419d0471c1b1146a30276a575
ripaZ <bandieramontel@gmail.com>
2021-05-05 17:42:14 +0200

    Aggiunte le funzioni di menu

--- exercise3/zmytest/test.cpp
+++ exercise3/zmytest/test.cpp
@@ -2,6 +2,7 @@
 #include "test.hpp"
 
 #include <iostream>
+#include <iomanip>
 #include <random>
 
 void StructureSelection(char& iInput, char& tInput) {
@@ -17,12 +18,12 @@ void StructureSelection(char& iInput, char& tInput) {
         iInput = std::toupper(iInput);
     }
 
-    std::cout<< std::endl <<"Selezionare il tipo di dato da trattare:"<< std::endl <<"    [I]nt    [D]ouble    [S]tring..."<< std::endl;
+    std::cout<< std::endl <<"Selezionare il tipo di dato da trattare:"<< std::endl <<"    [I]nt    [F]loat    [S]tring..."<< std::endl;
     std::cin>>tInput;
     tInput = std::toupper(tInput);
-    while(tInput != 'I' && tInput != 'D' && tInput != 'S')
+    while(tInput != 'I' && tInput != 'F' && tInput != 'S')
     {
-        std::cout<< std::endl <<"Inserire uno tra: I, D, S..."<< std::endl;
+        std::cout<< std::endl <<"Inserire uno tra: I, F, S..."<< std::endl;
         std::cin.clear();
         std::cin>>tInput;
         tInput = std::toupper(tInput);
@@ -30,7 +31,7 @@ void StructureSelection(char& iInput, char& tInput) {
 }
 
 template <typename Data>
-void RandPopulateVecInt(lasd::Vector<Data>& vec, const unsigned long& n) {
+void RandPopulateInt(lasd::Vector<Data>& vec, const unsigned long& n) {
     std::default_random_engine gen(std::random_device{}());
     std::uniform_int_distribution<Data> dist(1, 100);
     for (unsigned long i=0; i<n; i++)
@@ -38,7 +39,7 @@ void RandPopulateVecInt(lasd::Vector<Data>& vec, const unsigned long& n) {
 }
 
 template <typename Data>
-void RandPopulateVecReal(lasd::Vector<Data>& vec, const unsigned long& n) {
+void RandPopulateReal(lasd::Vector<Data>& vec, const unsigned long& n) {
     std::default_random_engine gen(std::random_device{}());
     std::uniform_real_distribution<Data> dist(1, 99);
     for(unsigned long i=0; i<n; i++)
@@ -46,7 +47,7 @@ void RandPopulateVecReal(lasd::Vector<Data>& vec, const unsigned long& n) {
 }
 
 template <typename Data>
-void RandPopulateVecString(lasd::Vector<Data>& vec, const unsigned long& n) {
+void RandPopulateString(lasd::Vector<Data>& vec, const unsigned long& n) {
     std::default_random_engine gen(std::random_device{}());
     std::uniform_int_distribution<int> dist(97, 122);
     std::default_random_engine lenGen(std::random_device{}());
@@ -56,6 +57,147 @@ void RandPopulateVecString(lasd::Vector<Data>& vec, const unsigned long& n) {
         for (int j=0; j<(lenDist(lenGen)); j++)
             vec[i].append(1, static_cast<char>(dist(gen)));
     }
+}
+
+template <typename Data>
+void MapPrint(const Data& dat, void* _) {
+  std::cout<< std::setprecision(7) << dat << " ";
+}
+
+template <typename Data>
+void StructurePrint(lasd::BinaryTree<Data>& bt) {
+    char choice;
+    std::cout<< std::endl <<"Selezionare un ordine per la stampa degli elementi: " << std::endl << "[P]reOrder    P[O]stOrder    [I]nOrder    [B]readth    " << std::endl;
+    std::cin.clear();
+    std::cin>>choice;
+    choice = std::toupper(choice);
+    while (choice != 'P' && choice != 'O' && choice != 'I' && choice != 'B')
+    {
+        std::cout<< std::endl <<"Inserire uno tra P, O, I oppure B..."<< std::endl;
+        std::cin.clear();
+        std::cin>>choice;
+        choice = std::toupper(choice);
+    }
+    if (choice == 'P')
+    {
+        std::cout<< std::endl <<"Stampa degli elementi nella struttura in Pre Order"<< std::endl;
+        bt.MapPreOrder(&MapPrint<Data>, 0);
+    }
+    else if (choice == 'O')
+    {
+        std::cout<< std::endl <<"Stampa degli elementi nella struttura in Post Order"<< std::endl;
+        bt.MapPostOrder(&MapPrint<Data>, 0);
+    }
+    else if (choice == 'I')
+    {
+        std::cout<< std::endl <<"Stampa degli elementi nella struttura in In Order"<< std::endl;
+        bt.MapInOrder(&MapPrint<Data>, 0);
+    }
+    else if (choice == 'B')
+    {
+        std::cout<< std::endl <<"Stampa degli elementi nella struttura in Ampiezza"<< std::endl;
+        bt.MapBreadth(&MapPrint<Data>, 0);
+    }
+}
+
+template <typename Data>
+void ElemExists(const lasd::FoldableContainer<Data>& con) {
+    Data elem;
+    do
+    {
+        std::cout<< std::endl << "Verificare l'esistenza di quale elemento?" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin>>elem;
+    } while (std::cin.fail());
+        std::cout<< std::endl <<"L'elemento "<< elem << (con.Exists(elem) ? "" : " non") << " è presente." << std::endl;
+}
+
+template <typename Data>
+void AuxFoldProd(const Data& dat, const void* val, void* prod) {
+    if (dat < *((int*)val))
+        *((int*)prod) = *((int*)prod)*dat;
+}
+
+template <typename Data>
+void SmallerThanNProd(const lasd::FoldableContainer<Data>& con) {
+    int n;
+    int prod = 1;
+    do
+    {
+        std::cout<< std::endl << "Moltiplicare gli elementi minori di quale numero?" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin>>n;
+    } while (std::cin.fail());
+    con.FoldPreOrder(&AuxFoldProd<Data>, &n, &prod);
+    std::cout<< std::endl << "Il prodotto richiesto è: " << prod << std::endl;
+}
+
+template <typename Data>
+void AuxFoldSum(const Data& dat, const void* val, void* sum) {
+    if (dat > *((float*)val))
+        *((float*)sum) += dat;
+}
+
+template <typename Data>
+void BiggerThanNSum(const lasd::FoldableContainer<Data>& con) {
+    float n;
+    float sum = 0;
+    do
+    {
+        std::cout<< std::endl << "Sommare gli elementi maggiori di quale numero?" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin>>n;
+    } while (std::cin.fail());
+    con.FoldPreOrder(&AuxFoldSum<Data>, &n, &sum);
+    std::cout<< std::endl << "La somma richiesta è: " << sum << std::endl;
+}
+
+template <typename Data>
+void AuxFoldConcat(const std::string& dat, const void* val, void* reString) {
+    if(dat.length() <= *((int*)val))
+        ((std::string*)reString)->append(dat);
+}
+
+template <typename Data>
+void ShorterThanNConcat(const lasd::FoldableContainer<Data>& con) {
+    int n;
+    std::string reString{};
+    do
+    {
+        std::cout<< std::endl << "Concatenare le stringhe con lunghezza minore di quale numero?" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin>>n;
+    } while (std::cin.fail());
+    con.FoldPreOrder(&AuxFoldConcat<Data>, &n, &reString);
+    std::cout<< std::endl << "La stringa ottenuta è: " << reString << std::endl;
+}
+
+template <typename Data>
+void MapTriple(Data& dat, void* _) {
+  dat *= 3;
+}
+
+template <typename Data>
+void MapCube(Data& dat, void* _) {
+    dat = dat*dat*dat;
+}
+
+template <typename Data>
+void MapInsert(Data& dat, void* str) {
+    dat.insert(0, *(std::string*)str);
+}
+
+template <typename Data>
+void StringInsert(lasd::MappableContainer<Data>& con) {
+    std::string str;
+    std::cout<< std::endl << "Inserisci la stringa da inserire in testa ad ogni elemento:" << std::endl;
+    std::cin.clear();
+    std::cin>>str;
+    con.MapPreOrder(&MapInsert<Data>, &str);
 }
 
 void PrintMenu(char& choice) {
@@ -99,7 +241,132 @@ void testMenu() {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cin>>n;
         } while (std::cin.fail());
-        
+        if (iInput == 'V')
+        {
+            if (tInput == 'I')
+            {
+                lasd::Vector<int> vec(n);
+                RandPopulateInt(vec, n);
+                lasd::BinaryTreeVec<int> btV(vec);
+                while (!end)
+                {
+                    PrintMenu(choice);
+                    if (choice == 'V')
+                        StructurePrint(btV);
+                    else if (choice == 'E')
+                        ElemExists(btV);
+                    else if (choice == 'F')
+                        SmallerThanNProd(btV);
+                    else if (choice == 'M')
+                        btV.MapPreOrder(&MapTriple<int>, 0);
+                    else if (choice == 'Q')
+                        end = true;
+                }
+            }
+            else if (tInput == 'F')
+            {
+                lasd::Vector<float> vec(n);
+                RandPopulateReal(vec, n);
+                lasd::BinaryTreeVec<float> btV(vec);
+                while (!end)
+                {
+                    PrintMenu(choice);
+                    if (choice == 'V')
+                        StructurePrint(btV);
+                    else if (choice == 'E')
+                        ElemExists(btV);
+                    else if (choice == 'F')
+                        BiggerThanNSum(btV);
+                    else if (choice == 'M')
+                        btV.MapPostOrder(&MapCube<float>, 0);
+                    else if (choice == 'Q')
+                        end = true;
+                }
+            }
+            else if (tInput == 'S')
+            {
+                lasd::Vector<std::string> vec(n);
+                RandPopulateString(vec, n);
+                lasd::BinaryTreeVec<std::string> btV(vec);
+                while (!end)
+                {
+                    PrintMenu(choice);
+                    if (choice == 'V')
+                        StructurePrint(btV);
+                    else if (choice == 'E')
+                        ElemExists(btV);
+                    else if (choice == 'F')
+                        ShorterThanNConcat(btV);
+                    else if (choice == 'M')
+                        StringInsert(btV);
+                    else if (choice == 'Q')
+                        end = true;
+                }
+            }
+        }
+        else if (iInput == 'L')
+        {
+            if (tInput == 'I')
+            {
+                lasd::Vector<int> vec(n);
+                RandPopulateInt(vec, n);
+                lasd::BinaryTreeLnk<int> btL(vec);
+                while (!end)
+                {
+                    PrintMenu(choice);
+                    if (choice == 'V')
+                        StructurePrint(btL);
+                    else if (choice == 'E')
+                        ElemExists(btL);
+                    else if (choice == 'F')
+                        SmallerThanNProd(btL);
+                    else if (choice == 'M')
+                        btL.MapPreOrder(&MapTriple<int>, 0);
+                    else if (choice == 'Q')
+                        end = true;
+                }
+            }
+            else if (tInput == 'F')
+            {
+                lasd::Vector<float> vec(n);
+                RandPopulateReal(vec, n);
+                lasd::BinaryTreeLnk<float> btL(vec);
+                while (!end)
+                {
+                    PrintMenu(choice);
+                    if (choice == 'V')
+                        StructurePrint(btL);
+                    else if (choice == 'E')
+                        ElemExists(btL);
+                    else if (choice == 'F')
+                        BiggerThanNSum(btL);
+                    else if (choice == 'M')
+                        btL.MapPostOrder(&MapCube<float>, 0);
+                    else if (choice == 'Q')
+                        end = true;
+                }
+            }
+            else if (tInput == 'S')
+            {
+                lasd::Vector<std::string> vec(n);
+                RandPopulateString(vec, n);
+                lasd::BinaryTreeLnk<std::string> btL(vec);
+                while (!end)
+                {
+                    PrintMenu(choice);
+                    if (choice == 'V')
+                        StructurePrint(btL);
+                    else if (choice == 'E')
+                        ElemExists(btL);
+                    else if (choice == 'F')
+                        ShorterThanNConcat(btL);
+                    else if (choice == 'M')
+                        StringInsert(btL);
+                    else if (choice == 'Q')
+                        end = true;
+                }
+            }
+        }
     }
     else
         std::cout << std::endl << "Goodbye!" << std::endl;
